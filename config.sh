c7fd7f9ed9f3ffcfe358aeee6250b1914a471c1f
petronijevicm <71526010+petronijevicm@users.noreply.github.com>
2020-12-17 19:14:29 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,10 +3,10 @@ cd ~
 sudo pacman -Syu
 sudo pacman -Sy coreutils entr git fish yay ansible neovim brave alacritty qemu virt-manager ghidra tmux htop
 clear
-echo downloading configuration files
-clear
+
+chsh -s /bin/fish
 
 cat /tmp/configuration/alacritty.yml > ~/.config/alacritty/alacritty.yml
 cat /tmp/configuration/config.fish > ~/.config/fish/config.fish
 cat /tmp/configuration/init.vim > ~/.config/nvim/init.vim
-
+clear && echo Finished with confihuration
