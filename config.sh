564cb19e0328b8ba8d7387c8cdd9aed4d3f4fecb
petronijevicm <71526010+petronijevicm@users.noreply.github.com>
2021-01-08 22:01:52 +0100

    comment out  application installs

--- config.sh
+++ config.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd ~
+cd $HOME
 sudo pacman -Syu
-sudo pacman -S coreutils entr git zsh yay ansible neovim brave qemu virt-manager calcurse tmux htop tlp zathura mpv youtube-dl youtube-viewer newsboat sxiv foliate meld
+#sudo pacman -S git zsh yay bspwm sxhkd brave calcurse newsboat tlp youtube-dl youtube-viewer
+#sudo pacman -S zathura sxiv mpv foliate neovim
+#sudo pacman -S qemu virt-manager tmux htop ansible meld entr tcc
+
 yay aliasrc
 
 chsh -s /bin/zsh
@@ -17,6 +20,4 @@ cat /tmp/configuration/config >~/.config/newsboat
 clear && echo Finished with confihuration
 
 git clone https://github.com/zdharma/fast-syntax-highlighting ~/Git
-echo "source ~/Git/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" >> ~/.zshrc
-
-echo "exec dwm" >> $HOME/.xinitrc
+echo "source ~/Git/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" >> ~/.config/zsh/.zshrc
