9e6ef079049b654e88ce3745c74e7fb794969081
petronijevicm <71526010+petronijevicm@users.noreply.github.com>
2021-04-22 19:17:03 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd $HOME
 
+#sudo dnf update -y
+#sudo dnf insall fish git browser tlp tcc ostree libappstream-glib appstream-glib -y
 sudo apt update
-sudo apt insall fish git browser tlp tcc ostree libappstream-glib appstream-glib
+sudo apt insall fish git browser tlp tcc ostree libappstream-glib appstream-glib -y
+sudo apt upgrade
 
 sudo tlp start
 
@@ -28,6 +31,24 @@ flatpak install flathub org.onlyoffice.desktopeditors -y --user
 mkdir ~/Git && cd ~/Git
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 git clone https://github.com/refi64/pakitheme.git
-cd auto-cpufreq && sudo ./auto-cpufreq-installer
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+cd WhiteSur-gtk-theme/ && ./install.sh -c dark -c light && ./tweaks.sh -f
+
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+cd ~/Git/WhiteSur-icon-theme && ./install.sh
+
+cd ~/Git/auto-cpufreq/ && sudo ./auto-cpufreq-installer
 sudo auto-cpufreq --install
 
+
+#sudo dnf install dnf-plugins-core
+#sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+#sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+#sudo dnf install brave-browser
+
+
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
