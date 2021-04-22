0f61a1a56833e9aff71f2c5b70f4ed45b4442193
petronijevicm <71526010+petronijevicm@users.noreply.github.com>
2021-04-22 19:22:35 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -52,3 +52,10 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
+
+cd ~/Git
+wget https://s3.amazonaws.com/winds-2.0-releases/releases/Winds-3.2.0.AppImage
+
+
+
+
