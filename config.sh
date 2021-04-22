e323102a512376ce2c05174c7e12f589a21d0846
petronijevicm <71526010+petronijevicm@users.noreply.github.com>
2021-04-22 19:00:50 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 cd $HOME
-sudo pacman -Syu
 
+sudo apt update
 sudo apt insall fish git browser tlp tcc ostree libappstream-glib appstream-glib
 
-
-chsh -s /bin/fish
 sudo tlp start
 
 cat /tmp/configuration/init.vim > ~/.config/nvim/init.vim
@@ -18,18 +16,16 @@ flatpak install flathub com.transmissionbt.Transmission -y --user
 flatpak install flathub com.tutanota.Tutanota -y --user
 flatpak install flathub com.vscodium.codium -y --user
 flatpak install flathub fr.romainvigier.MetadataCleaner -y --user
-flatpak install flathub io.github.liberodark.OpenDrive -y --user
 flatpak install flathub org.blender.Blender -y --user
 flatpak install flathub org.glimpse_editor.Glimpse -y --user
 flatpak install flathub org.gnome.Totem -y --user
 flatpak install flathub org.gnome.meld -y --user
 flatpak install flathub org.onlyoffice.desktopeditors -y --user
-flatpak install flathub com.axosoft.GitKraken -y --user
+flatpak install flathub com.viber.Viber -y --user
+flatpak install flathub org.onlyoffice.desktopeditors -y --user
 
-cd ~/Pictures
-git clone https://github.com/eallion/Big-Sur-Ubuntu.git
 
-mkdir ~/Git && cd Git
+mkdir ~/Git && cd ~/Git
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 git clone https://github.com/refi64/pakitheme.git
 cd auto-cpufreq && sudo ./auto-cpufreq-installer
